54a822bbf313e2ac8473a284bea8f44c04c9a0a3
Asabeneh <asabeneh@gmail.com>
2019-06-07 15:23:03 +0300

    documentation modified

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const mongoose = require('mongoose');
 const studentRoutes = require('./routes');
 const displayDayTime = require('./shared/display-day-time');
 
-app.use((req, res, next) => {
-  const time = displayDayTime();
-  console.log(req.url);
-  console.log(`The user is checking ${req.url} at ${time}`);
-  next();
-});
+// app.use((req, res, next) => {
+//   const time = displayDayTime();
+//   console.log(req.url);
+//   console.log(`The user is checking ${req.url} at ${time}`);
+//   next();
+// });
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, err => {
   if (err) {
     console.log(err);
